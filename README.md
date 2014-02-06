d4943dc3708fb4ee2d922ca275120bc566de02ed
mhkeller <michael.keller@gmail.com>
2014-02-06 15:06:44 -0500

    some changes

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Scratch'n Sniff
 
-A combination of the Cheerio and Request libraries to make a nodejs scraper that doesn't have the same memory leak issues as jsdom-based scrapers like node-scraper. Also allows for rate limiting and a `.done()` method when all of your pages are scraped.
+A mostly useless scraping library.
+
+Scratch'n Sniif is a combination of the Cheerio and Request libraries to make a nodejs scraper that doesn't have the same memory leak issues as jsdom-based scrapers like node-scraper. Also allows for rate limiting and a `.done()` method when all of your pages are scraped.
 
 Can output `json` (default) or `csv` if json is properly formatted
--- examples/noms.js
+++ examples/noms.js
@@ -1,22 +1,24 @@
 var fs        = require('fs'),
 		Scraper   = require('../lib/scratch-n-sniff.js'),
 		_         = require('underscore');
- 
+
 var years = [2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013];
 
+// TODO find a balance between chained method calls and feeding params to a single function
 var scrape = Scraper()
 									.data(years)
 									.url('http://www.imdb.com/event/ev0000003/', function(d) { return d })
 									.extractor(parsePage, function(d) { return d } )
 									.rateLimit(1000)
-									.format('csv'); // Defaults to json
+									.format('csv') // Defaults to json
+									.start(onDone);
 
 
 function parsePage($, year, callback, status){
 	console.log(status) // Print which page we're fetching
 
-	var movies   = []; // This scraper is getting multiple objects per page. You could also give the callback an object and it will still produce a csv
-	
+	var movies = {}; // This scraper is getting multiple objects per page. You could also give the callback an object and it will still produce a csv
+
 	$('[class^=alt] strong').each(function(index, el){
 		var movie = {};
 
@@ -24,19 +26,19 @@ function parsePage($, year, callback, status){
 				title = $(el).find('a').html().replace('&#xE9;', 'é').replace('&#x27;' , '\'').replace('&#x26;', '&');
 
 		// Only include movies we haven't already encountered on this page
-		if (_.chain(movies).map(function(m){ return _.values(m)}).flatten().value().indexOf(id) == -1){
+		if (!movie[id]){
 			movie.year   = year;
 			movie.imdbid = id;
 			movie.title  = title;
-			movies.push(movie);
+
+			movies[id] = movie;
 		}
 	})
 
-	callback(movies)
+	callback(_.values(movies))
 }
 
 
-// When all done
-scrape.done(function(results){
+function onDone(results){
 	console.log(results)
-})
+}
