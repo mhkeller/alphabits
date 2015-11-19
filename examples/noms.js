b6be85c3a8519de3de11bbeb2391ea23cfc26a3a
Michael Keller <mhkeller@users.noreply.github.com>
2015-11-19 15:59:32 -0500

    Update noms.js

--- examples/noms.js
+++ examples/noms.js
@@ -1,4 +1,4 @@
-var Scraper = require('../lib/index.js');
+var Scraper = require('../lib/index.js'); // In production, replace this line with `var Scraper = require('alphabits')`
 var _ = require('underscore');
 
 var years = [2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013];
